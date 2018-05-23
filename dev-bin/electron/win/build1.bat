b46edc051789c3e0e6cd3324bf8d00d0314a1c4e
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-23 17:02:23 +0800

    0523 改用node.js來打包看看

--- dev-bin/electron/win/build1.bat
+++ dev-bin/electron/win/build1.bat
@@ -1,4 +1,4 @@
 cd %~dp0
-cd ..\www
+cd ..\..\..\www
 rmdir /S /Q hybird-app-win32-x64
 npm run package-win
---
+++ www/build1.bat
@@ -0,0 +1,3 @@
+npm run package-win
+npm run package
+ && rm -rf hybird-app-win32-x64/resources/app
