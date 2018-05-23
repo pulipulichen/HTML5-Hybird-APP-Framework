fb0c3cbde5b59bae2c97042cad5cb95c49cda297
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-23 23:58:01 +0800

    0523 這樣做分享可以嗎？

--- www/helpers/hybird-app/hybird_app_helper.js
+++ www/helpers/hybird-app/hybird_app_helper.js
@@ -1,74 +1,18 @@
 hybird_app_helper = {
-    open_window: function (_link, _target) {
-        if (_target === undefined) {
-            _target = "_blank";
-        }
-
-        if (typeof (ipcRenderer) === "undefined") {
-            window.open(_link, _target);
-        } else {
-            ipcRenderer.send('open_window', _link);
-        }
-    },
-    retrieve_web: function (_url, _method, _send_data, _referer, _callback) {
-        if (typeof (_callback) !== "function") {
-            return false;
-        }
-
-        var _send_data_string = [];
-        for (var _key in _send_data) {
-            _send_data_string.push(_key + "=" + _send_data[_key]);
-        }
-        _send_data_string = _send_data_string.join("&");
-
-        var _callback_id = "retrieve_web_callback_" + this.create_uuid();
-
-        ipcRenderer.on(_callback_id, function (event, _response, _status) {
-            _callback(_response, _status);
-        });
-
-        ipcRenderer.send('retrieve_web', _url, _method, _send_data, _referer, _callback_id);
-    },
-    uuid: 0,
-    create_uuid: function () {
-        var _uuid = this.uuid;
-        this.uuid++;
-        return _uuid;
-    },
-    set_item: function (_key, _value) {
-        if (typeof (_value) !== 'string' && typeof (_value) !== 'number') {
-            _value = JSON.stringify(_value);
-        }
-        ipcRenderer.send('set_item', _key, _value);
-    },
-    get_item: function (_key, _callback) {
-        var _callback_id = "get_item_callback_" + this.create_uuid();
-        ipcRenderer.on(_callback_id, function (event, _value) {
-            //console.log("有回來嗎");
-            _callback(_value);
-        });
-        //console.log([_callback_id, _key]);
-        ipcRenderer.send('get_item', _key, _callback_id);
-    },
-    console_log: function (_msg) {
-        console.log(_msg);
-    },
-    mode: 'web',
+    platform: 'web',
     detect_mode: function () {
-        if (ELECTRON_ENABLE === true) {
-            this.mode = 'electron';
+        if (this.is_electron() === true) {
+            this.platform = 'electron';
         }
         else if (this.isPhoneGap()) {
-            this.mode = 'mobile';
-        }
-    },
-    setup_mode: function () {
-        if (ELECTRON_ENABLE === true) {
-            this.mode = 'electron';
+            this.platform = 'mobile';
         }
         else {
-            this.mode = 'web';
+            this.platform = "web";
         }
+        $(function () {
+            $("body").addClass("platform-" + this.platform);
+        });
     },
     /**
      * @author https://stackoverflow.com/a/13252184
@@ -78,6 +22,48 @@ hybird_app_helper = {
         return (window.cordova || window.PhoneGap || window.phonegap) 
         && /^file:\/{3}[^\/]/i.test(window.location.href) 
         && /ios|iphone|ipod|ipad|android/i.test(navigator.userAgent);
+    },
+
+    is_electron: function () {
+        try {
+            window.nodeRequire = require;
+            //delete window.require;
+            delete window.exports;
+            delete window.module;
+            electron = require('electron');
+            ipcRenderer = electron.ipcRenderer;
+            return true;
+        } catch (_e) {
+            return false;
+        }
+    },
+    
+    // ----------------------
+    
+    open_window: function (_link, _target) {
+        if (this.platform === "electron") {
+            ipcRenderer.send('open_window', _link);
+        }
+        else if (this.platform === "mobile") {
+            window.open(_link, "_system");
+        }
+        else {
+            if (_target === undefined) {
+                _target = "_blank";
+            }
+            window.open(_link, _target);
+        }
+    },
+    
+    uuid: 0,
+    create_uuid: function () {
+        var _uuid = this.uuid;
+        this.uuid++;
+        return _uuid;
+    },
+    
+    console_log: function (_msg) {
+        console.log(_msg);
     },
     /**
      * 
@@ -89,13 +75,28 @@ hybird_app_helper = {
      */
     save_as: function (_filename, _content, _mime, _filters) {
         //this.console_log([_filename, _content]);
-        switch (this.mode) {
-            case 'web': 
-                var blob = this.b64toFile(_content, _filename, _mime);
-                saveAs(blob, _filename);
-                break;
+        switch (this.platform) {
             case 'electron':
                 ipcRenderer.send('save_file', _filename, JSON.stringify(_filters), _content, true);
+                break;
+            case 'mobile':
+                //cordova.file.cacheDirectory
+                var _filepath = cordova.file.cacheDirectory + "/" + _filename;
+                window.resolveLocalFileSystemURL(cordova.file.cacheDirectory, function (dir) {
+                    //console.log("got main dir",dir);
+                    dir.getFile(_filename, {create: true}, function (file) {
+                        //console.log("got the file", file);
+                        file.createWriter(function (fileWriter) {
+                            var blob = this.b64toFile(_content, _filename, _mime);
+                            fileWriter.write(blob);
+                            window.plugins.socialsharing.share(_filename, null, _filepath);
+                        });
+                    });
+                });
+                break;
+            default:    // 'web'
+                var blob = this.b64toFile(_content, _filename, _mime);
+                saveAs(blob, _filename);
                 break;
         }
     },
@@ -122,7 +123,43 @@ hybird_app_helper = {
         }
         var file = new File(byteArrays, filename, {type: contentType});
         return file;
-    }
+    },
+    
+    // -----------
+    retrieve_web: function (_url, _method, _send_data, _referer, _callback) {
+        if (typeof (_callback) !== "function") {
+            return false;
+        }
+
+        var _send_data_string = [];
+        for (var _key in _send_data) {
+            _send_data_string.push(_key + "=" + _send_data[_key]);
+        }
+        _send_data_string = _send_data_string.join("&");
+
+        var _callback_id = "retrieve_web_callback_" + this.create_uuid();
+
+        ipcRenderer.on(_callback_id, function (event, _response, _status) {
+            _callback(_response, _status);
+        });
+
+        ipcRenderer.send('retrieve_web', _url, _method, _send_data, _referer, _callback_id);
+    },
+    set_item: function (_key, _value) {
+        if (typeof (_value) !== 'string' && typeof (_value) !== 'number') {
+            _value = JSON.stringify(_value);
+        }
+        ipcRenderer.send('set_item', _key, _value);
+    },
+    get_item: function (_key, _callback) {
+        var _callback_id = "get_item_callback_" + this.create_uuid();
+        ipcRenderer.on(_callback_id, function (event, _value) {
+            //console.log("有回來嗎");
+            _callback(_value);
+        });
+        //console.log([_callback_id, _key]);
+        ipcRenderer.send('get_item', _key, _callback_id);
+    },
 };
 
 setTimeout(function () {
